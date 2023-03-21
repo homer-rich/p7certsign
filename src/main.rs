bc84b96f10773dcf1374a8f3fcf36fff5d963c75
Homer on Desktop <homer.rich@gmail.com>
2023-03-20 21:48:59 -0400

    Notquiteworking_morework_necessary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 #![allow(unused_imports)]
 use std::{ffi::c_void, mem::transmute, ptr, ops::BitOr};
 use windows::{
-    core::*, Win32::{Security::Cryptography::{*, UI::{CryptUIWizDigitalSign, CERT_SELECT_STRUCT_W, CSS_ENABLETEMPLATE}}, System::{Threading::{CreateEventW, WaitForSingleObject, SetEvent}, LibraryLoader::{GetModuleHandleW, GetProcAddress, LoadLibraryW}}, Foundation::{CloseHandle, WPARAM, LPARAM, HWND}, UI::WindowsAndMessaging::MessageBoxW},
+    core::*, Win32::{
+        Security::Cryptography::{
+        UI::{CryptUIWizDigitalSign, CERT_SELECT_STRUCT_W, CSS_ENABLETEMPLATE, self}, 
+        CERT_STORE_PROV_SYSTEM_W, CERT_QUERY_ENCODING_TYPE, HCRYPTPROV_LEGACY, CERT_SYSTEM_STORE_CURRENT_USER_ID, 
+        CertOpenStore, CERT_SYSTEM_STORE_LOCATION_SHIFT, CERT_STORE_PROV_MEMORY, CERT_OPEN_STORE_FLAGS, CERT_CONTEXT, 
+        CRYPT_SIGN_MESSAGE_PARA, X509_ASN_ENCODING, PKCS_7_ASN_ENCODING, CryptSignMessage, CertFreeCertificateContext, 
+        CertCloseStore, CRYPT_ALGORITHM_IDENTIFIER, CryptAcquireCertificatePrivateKey, CRYPT_ACQUIRE_ALLOW_NCRYPT_KEY_FLAG, CRYPT_ACQUIRE_CACHE_FLAG, CERT_KEY_SPEC, CRYPT_INTEGER_BLOB
+        }, System::{
+            Threading::{CreateEventW, WaitForSingleObject, SetEvent}, LibraryLoader::{GetModuleHandleW, GetProcAddress, LoadLibraryW}
+        }, Foundation::{
+            CloseHandle, WPARAM, LPARAM, HWND, BOOL
+        }, 
+        UI::WindowsAndMessaging::MessageBoxW
+    },
 };
 
 // https://learn.microsoft.com/en-us/windows/win32/seccrypto/cryptography-functions
@@ -54,7 +67,8 @@ fn main() -> Result<()> {
             // code signing 
             // szPurposeOid: s!("1.3.6.1.5.5.7.3.3"),
             // on our encryption certs
-            szPurposeOid: s!("1.3.6.1.4.1.311.10.3.12"),
+            //szPurposeOid: s!("1.3.6.1.4.1.311.10.3.12"),
+            szPurposeOid: s!(""),
             cCertContext: 0,
             arrayCertContext: &mut fresh_cert,
             lCustData: windows::Win32::Foundation::LPARAM(0),
@@ -80,16 +94,30 @@ fn main() -> Result<()> {
             ::core::mem::zeroed(),
         ); */
 
+        // Acquire Private Key for Certificate
+/*         let h_crypt_prov = ::core::mem::zeroed();
+        let mut pdw_key_spec:CERT_KEY_SPEC = windows::Win32::Security::Cryptography::CERT_KEY_SPEC(5000u32);
+        let mut pf_caller_free: BOOL = BOOL::default();
+        let _key_acquired = CryptAcquireCertificatePrivateKey (
+            fresh_cert,
+            CRYPT_ACQUIRE_CACHE_FLAG,
+            ::core::mem::zeroed(),
+            h_crypt_prov,
+            Some(&mut pdw_key_spec),
+            Some(&mut pf_caller_free),
+        ); */
+
         // Sign a file with the selected cert
         // https://learn.microsoft.com/en-us/windows/win32/seccrypto/example-c-program-signing-a-message-and-verifying-a-message-signature
-        let oid = "1.2.840.113549.1.1.5\0".to_owned().as_mut_ptr();
+        let oid = "1.2.840.113549.2.2\0".to_owned().as_mut_ptr();
+        let cst_oid = windows::core::PSTR::from_raw(oid);
         let crypt_sign_message_para = CRYPT_SIGN_MESSAGE_PARA {
-            cbSize: std::mem::size_of::<CRYPT_SIGN_MESSAGE_PARA>() as u32,
-            dwMsgEncodingType: PKCS_7_ASN_ENCODING.bitor(X509_ASN_ENCODING).0,
+            cbSize: u32::try_from(std::mem::size_of::<CRYPT_SIGN_MESSAGE_PARA>()).unwrap(),
+            dwMsgEncodingType: X509_ASN_ENCODING.0,
             pSigningCert: fresh_cert,
             HashAlgorithm: CRYPT_ALGORITHM_IDENTIFIER { 
-                            pszObjId: windows::core::PSTR::from_raw(oid), 
-                            Parameters: CRYPT_INTEGER_BLOB::default() },
+                pszObjId: cst_oid, 
+                Parameters: CRYPT_INTEGER_BLOB::default()},
             pvHashAuxInfo: ::core::mem::zeroed(),
             cMsgCert: 1,
             rgpMsgCert: &mut fresh_cert,
@@ -100,15 +128,13 @@ fn main() -> Result<()> {
             cUnauthAttr: 0,
             rgUnauthAttr: ::core::mem::zeroed(),
             dwFlags: 0,
-            dwInnerContentType: 0,
-        };
+            dwInnerContentType: 0};
 
-        let secret_message = "eessageToBeSigned\0";
+        let secret_message: PCSTR = s!("Secret Message");
         let sign_me:Vec<*const u8> = vec!(secret_message.as_ptr());
-        
-        let to_be_signed_sizes_array = vec!(50000 as u32);
-        let mut data_size: u32 = 50000;
-        let return_buffer = ::core::mem::zeroed();
+        let to_be_signed_sizes_array:Vec<u32> = vec!(u32::try_from(secret_message.as_bytes().len()).unwrap());
+        let mut data_size = 0;
+
         // First call sets up variables to receive the size of the signed data.
         let sign_success = CryptSignMessage(
             &crypt_sign_message_para,
@@ -116,11 +142,14 @@ fn main() -> Result<()> {
             1,
             Some(sign_me.as_ptr()),
             to_be_signed_sizes_array.as_ptr(),
-            //::core::mem::zeroed(),
-            return_buffer,
-            &mut data_size,
-        );
+            ::core::mem::zeroed(),
+            &mut data_size);
+
+
         println!("First call complete.");
+
+        let return_buffer: u32 = 50000;
+        let _return_pointer: *mut u8 = return_buffer as *mut u8;
         /* let pb_signed_message_blob: *mut u8 = ::core::mem::zeroed();
         let _sign_success_2 = CryptSignMessage(
             &crypt_sign_message_para,
