a07c2e6a88d021b987783fb50a16539765fe1aca
Homer on Desktop <homer.rich@gmail.com>
2023-04-13 08:31:07 -0400

    Ran rust formatter on current code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 #![allow(unused_imports)]
 #![allow(unused_mut)]
-use std::{ffi::{c_void, OsStr}, mem::transmute, fs::{File, DirEntry}, io::Write, path::Component, collections::HashMap};
-use windows::{
-    core::*, Win32::{
-        Security::Cryptography::{
-        UI::{CERT_SELECT_STRUCT_W, CSS_ENABLETEMPLATE, self}, 
-        CERT_STORE_PROV_SYSTEM_W, CERT_QUERY_ENCODING_TYPE, HCRYPTPROV_LEGACY, CERT_SYSTEM_STORE_CURRENT_USER_ID, 
-        CertOpenStore, CERT_SYSTEM_STORE_LOCATION_SHIFT, CERT_OPEN_STORE_FLAGS, CERT_CONTEXT, 
-        CRYPT_SIGN_MESSAGE_PARA, PKCS_7_ASN_ENCODING, CryptSignMessage, CertFreeCertificateContext, 
-        CertCloseStore, CRYPT_ALGORITHM_IDENTIFIER, CRYPT_INTEGER_BLOB, CERT_CHAIN_PARA, CERT_CHAIN_CONTEXT, CertFreeCertificateChain, CertCreateCertificateContext, X509_ASN_ENCODING, CERT_STORE_CERTIFICATE_CONTEXT, CryptStringToBinaryA, CRYPT_STRING_BASE64HEADER, CryptStringToBinaryW, CERT_STORE_PROV_MEMORY, CERT_STORE_ADD_REPLACE_EXISTING, CertAddCertificateContextToStore, CERT_STORE_SAVE_AS_PKCS7, CERT_STORE_SAVE_TO_FILENAME_W, CertSaveStore, HCERTSTORE, CERT_STORE_SAVE_TO_FILENAME_A
-        }, System::{
-            LibraryLoader::{GetProcAddress, LoadLibraryW, FreeLibrary}, 
-            Memory::{HeapAlloc, GetProcessHeap, HEAP_ZERO_MEMORY}
-        },  Storage::FileSystem::{
-            CreateFileW, FILE_GENERIC_WRITE, FILE_SHARE_WRITE, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 
-        }, Foundation::GetLastError
-    },
+use std::path::Path;
+use std::{
+    collections::HashMap,
+    ffi::{c_void, OsStr},
+    fs::{DirEntry, File},
+    io::Write,
+    mem::transmute,
+    path::Component,
 };
 use walkdir::WalkDir;
-use std::path::Path;
+use windows::{
+    core::*,
+    Win32::{
+        Foundation::GetLastError,
+        Security::Cryptography::{
+            CertAddCertificateContextToStore, CertCloseStore, CertCreateCertificateContext,
+            CertFreeCertificateChain, CertFreeCertificateContext, CertOpenStore, CertSaveStore,
+            CryptSignMessage, CryptStringToBinaryA, CryptStringToBinaryW, CERT_CHAIN_CONTEXT,
+            CERT_CHAIN_PARA, CERT_CONTEXT, CERT_OPEN_STORE_FLAGS, CERT_QUERY_ENCODING_TYPE,
+            CERT_STORE_ADD_REPLACE_EXISTING, CERT_STORE_CERTIFICATE_CONTEXT,
+            CERT_STORE_PROV_MEMORY, CERT_STORE_PROV_SYSTEM_W, CERT_STORE_SAVE_AS_PKCS7,
+            CERT_STORE_SAVE_TO_FILENAME_A, CERT_STORE_SAVE_TO_FILENAME_W,
+            CERT_SYSTEM_STORE_CURRENT_USER_ID, CERT_SYSTEM_STORE_LOCATION_SHIFT,
+            CRYPT_ALGORITHM_IDENTIFIER, CRYPT_INTEGER_BLOB, CRYPT_SIGN_MESSAGE_PARA,
+            CRYPT_STRING_BASE64HEADER, HCERTSTORE, HCRYPTPROV_LEGACY, PKCS_7_ASN_ENCODING,
+            UI::{self, CERT_SELECT_STRUCT_W, CSS_ENABLETEMPLATE},
+            X509_ASN_ENCODING,
+        },
+        Storage::FileSystem::{
+            CreateFileW, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_WRITE, FILE_SHARE_WRITE,
+        },
+        System::{
+            LibraryLoader::{FreeLibrary, GetProcAddress, LoadLibraryW},
+            Memory::{GetProcessHeap, HeapAlloc, HEAP_ZERO_MEMORY},
+        },
+    },
+};
 
 // https://learn.microsoft.com/en-us/windows/win32/seccrypto/cryptography-functions
 type CertSelectCertificateW = extern "stdcall" fn(*const CERT_SELECT_STRUCT_W);
@@ -27,30 +45,42 @@ fn main() -> Result<()> {
     unsafe {
         let mut fresh_cert: *mut CERT_CONTEXT = ::core::mem::zeroed();
         select_signing_cert(&mut fresh_cert)?;
-        let mut bundles:HashMap<String, HCERTSTORE> = HashMap::new();
+        let mut bundles: HashMap<String, HCERTSTORE> = HashMap::new();
         let mut current_dir_string: String;
-        
-        for entry in WalkDir::new("certificates").into_iter().filter_map(|e| e.ok()) {
+
+        for entry in WalkDir::new("certificates")
+            .into_iter()
+            .filter_map(|e| e.ok())
+        {
             if entry.path().is_dir() && entry.path().parent().ne(&Some(Path::new(""))) {
-                current_dir_string = entry.path().file_stem().unwrap().to_str().unwrap().to_string();
+                current_dir_string = entry
+                    .path()
+                    .file_stem()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
                 let mut temp_store = CertOpenStore(
                     CERT_STORE_PROV_MEMORY,
                     CERT_QUERY_ENCODING_TYPE::default(),
                     HCRYPTPROV_LEGACY::default(),
                     CERT_OPEN_STORE_FLAGS(0),
-                    ::core::mem::zeroed())?;
+                    ::core::mem::zeroed(),
+                )?;
                 bundles.insert(current_dir_string.clone(), temp_store);
 
                 for certificate_file in entry.path().read_dir().expect("read_dir call failure") {
                     let current_file = certificate_file.unwrap().path();
                     if current_file.is_file() && current_file.extension().unwrap() == "cer" {
-                        let cert_context = get_context_cert_file(&std::fs::read(current_file).unwrap())?;
+                        let cert_context =
+                            get_context_cert_file(&std::fs::read(current_file).unwrap())?;
                         let update_store = bundles.get(&current_dir_string).unwrap();
                         let test_add = CertAddCertificateContextToStore(
                             *update_store,
                             cert_context,
                             CERT_STORE_ADD_REPLACE_EXISTING,
-                            None);
+                            None,
+                        );
 
                         if test_add.as_bool() {
                             //println!("Successfully added a component to {}", &current_dir_string);
@@ -64,7 +94,7 @@ fn main() -> Result<()> {
             let mut file_name = "certificates_pkcs7_5_12_".to_owned();
             file_name.push_str(y.0.as_str());
             file_name.push_str(".p7b\0");
-            
+
             let file_name_ptr = PCSTR(file_name.as_ptr()).as_ptr();
             CertSaveStore(
                 y.1,
@@ -72,7 +102,8 @@ fn main() -> Result<()> {
                 CERT_STORE_SAVE_AS_PKCS7,
                 CERT_STORE_SAVE_TO_FILENAME_A,
                 file_name_ptr as _,
-                0);
+                0,
+            );
 
             if CertCloseStore(y.1, 0).as_bool() {
                 println!("Closed the {} memory_store", y.0);
@@ -84,19 +115,22 @@ fn main() -> Result<()> {
         if CertFreeCertificateContext(Some(fresh_cert)).as_bool() {
             println!("Closed the fresh_cert");
         }
-    } 
+    }
     Ok(())
 }
 
-pub unsafe fn select_signing_cert(fresh_cert:*mut *mut CERT_CONTEXT) -> Result<()>{
+pub unsafe fn select_signing_cert(fresh_cert: *mut *mut CERT_CONTEXT) -> Result<()> {
     let store_name = w!("My").as_ptr() as *const c_void;
 
     let mut personal_store = CertOpenStore(
         CERT_STORE_PROV_SYSTEM_W,
         CERT_QUERY_ENCODING_TYPE::default(),
         HCRYPTPROV_LEGACY::default(),
-        CERT_OPEN_STORE_FLAGS(CERT_SYSTEM_STORE_CURRENT_USER_ID << CERT_SYSTEM_STORE_LOCATION_SHIFT),
-        Some(store_name))?;
+        CERT_OPEN_STORE_FLAGS(
+            CERT_SYSTEM_STORE_CURRENT_USER_ID << CERT_SYSTEM_STORE_LOCATION_SHIFT,
+        ),
+        Some(store_name),
+    )?;
 
     let crypt_ui_instance = LoadLibraryW(w!("cryptdlg.dll"))?;
 
@@ -109,7 +143,7 @@ pub unsafe fn select_signing_cert(fresh_cert:*mut *mut CERT_CONTEXT) -> Result<(
         szTitle: w!("Certificate to Sign PKCS7"),
         cCertStore: 1,
         arrayCertStore: &mut personal_store,
-        // code signing 
+        // code signing
         // szPurposeOid: s!("1.3.6.1.5.5.7.3.3"),
         // on our encryption certs
         szPurposeOid: s!("1.3.6.1.4.1.311.10.3.12"),
@@ -123,42 +157,45 @@ pub unsafe fn select_signing_cert(fresh_cert:*mut *mut CERT_CONTEXT) -> Result<(
         hprov: 0,
     };
 
-    let cert_select_certificate_w: CertSelectCertificateW = transmute(
-        GetProcAddress(crypt_ui_instance, s!("CertSelectCertificateW")));
+    let cert_select_certificate_w: CertSelectCertificateW = transmute(GetProcAddress(
+        crypt_ui_instance,
+        s!("CertSelectCertificateW"),
+    ));
     cert_select_certificate_w(&cert_select_struct);
 
     // clean-up
-    if FreeLibrary(crypt_ui_instance).as_bool() { 
+    if FreeLibrary(crypt_ui_instance).as_bool() {
         println!("Closed the lib")
     };
     if CertCloseStore(personal_store, 0).as_bool() {
         println!("Closed the personal_store");
     }
-    if fresh_cert.is_null() { std::process::exit(1); }
+    if fresh_cert.is_null() {
+        std::process::exit(1);
+    }
     Ok(())
 }
 
 pub unsafe fn get_context_cert_file(cert_bytes: &[u8]) -> Result<*mut CERT_CONTEXT> {
-    let ret_val = CertCreateCertificateContext(
-        X509_ASN_ENCODING,
-        cert_bytes);
+    let ret_val = CertCreateCertificateContext(X509_ASN_ENCODING, cert_bytes);
     if !ret_val.is_null() {
         return Ok(ret_val);
     } else {
         let buf = convert_from_pem_to_der(cert_bytes)?;
-        
-        let ret_val = CertCreateCertificateContext(
-            X509_ASN_ENCODING,
-            &buf);
+
+        let ret_val = CertCreateCertificateContext(X509_ASN_ENCODING, &buf);
         if !ret_val.is_null() {
             return Ok(ret_val);
         } else {
-            return Err(Error::new(GetLastError().to_hresult(), HSTRING::from("Certificate context as NULL")));
+            return Err(Error::new(
+                GetLastError().to_hresult(),
+                HSTRING::from("Certificate context as NULL"),
+            ));
         }
     }
 }
 
-pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
+pub unsafe fn do_the_signing(fresh_cert: *mut CERT_CONTEXT) {
     // Grab certificate chain
     let cert_chain_parameter = CERT_CHAIN_PARA {
         cbSize: u32::try_from(std::mem::size_of::<CERT_CHAIN_PARA>()).unwrap(),
@@ -166,7 +203,7 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
     };
 
     let mut fresh_chain: *mut CERT_CHAIN_CONTEXT = ::core::mem::zeroed();
-    windows::Win32::Security::Cryptography::CertGetCertificateChain (
+    windows::Win32::Security::Cryptography::CertGetCertificateChain(
         None,
         fresh_cert,
         ::core::mem::zeroed(),
@@ -180,8 +217,8 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
     let test2 = std::slice::from_raw_parts(test1, 3 as _);
     // get second cert in data array spot 0 is the default cert, slot 1 is next up in the chain
     /* let _test3 = std::slice::from_raw_parts(
-        (*(test2[1].pCertContext)).pbCertEncoded, 
-        (*(test2[1].pCertContext)).cbCertEncoded as _); */
+    (*(test2[1].pCertContext)).pbCertEncoded,
+    (*(test2[1].pCertContext)).cbCertEncoded as _); */
 
     let test4 = test2[1].pCertContext.cast_mut();
     let test5 = *(test2[2].pCertContext);
@@ -189,7 +226,7 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
     std::fs::write("root.p7b", cert_raw_parts).unwrap();
     // get chain size
     dbg!((**(*fresh_chain).rgpChain).cElement);
-    let mut certs_in_signature:Vec<*mut CERT_CONTEXT> = vec!(test4, fresh_cert);
+    let mut certs_in_signature: Vec<*mut CERT_CONTEXT> = vec![test4, fresh_cert];
 
     // Sign a file with the selected cert
     // https://learn.microsoft.com/en-us/windows/win32/seccrypto/example-c-program-signing-a-message-and-verifying-a-message-signature
@@ -199,9 +236,10 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
         cbSize: u32::try_from(std::mem::size_of::<CRYPT_SIGN_MESSAGE_PARA>()).unwrap(),
         dwMsgEncodingType: PKCS_7_ASN_ENCODING.0,
         pSigningCert: fresh_cert,
-        HashAlgorithm: CRYPT_ALGORITHM_IDENTIFIER { 
-                        pszObjId: windows::core::PSTR::from_raw(OID.cast_mut()), 
-                        Parameters: CRYPT_INTEGER_BLOB::default() },
+        HashAlgorithm: CRYPT_ALGORITHM_IDENTIFIER {
+            pszObjId: windows::core::PSTR::from_raw(OID.cast_mut()),
+            Parameters: CRYPT_INTEGER_BLOB::default(),
+        },
         pvHashAuxInfo: ::core::mem::zeroed(),
         cMsgCert: 2,
         rgpMsgCert: certs_in_signature.as_mut_ptr(),
@@ -212,10 +250,15 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
         cUnauthAttr: 0,
         rgUnauthAttr: ::core::mem::zeroed(),
         dwFlags: 0,
-        dwInnerContentType: 0 };
+        dwInnerContentType: 0,
+    };
 
     let mut sha256_string = String::new();
-    for entry in WalkDir::new(".").max_depth(1).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(".")
+        .max_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
         if entry.path().is_file() && entry.path().extension().unwrap_or_default() == "p7b" {
             let input = std::path::Path::new(entry.path().as_os_str());
             let path_string = entry.path().file_name().unwrap().to_str().unwrap();
@@ -227,10 +270,11 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
     sha256_string.push_str("\0");
 
     let s_val = PCSTR(sha256_string.as_ptr());
-    let sign_me:Vec<*const u8> = vec!(s_val.as_ptr());
-    let to_be_signed_sizes_array:Vec<u32> = vec!(
+    let sign_me: Vec<*const u8> = vec![s_val.as_ptr()];
+    let to_be_signed_sizes_array: Vec<u32> = vec![
         //u32::try_from(secret_message.as_bytes().len()).unwrap(),);
-        u32::try_from(s_val.as_bytes().len()).unwrap());
+        u32::try_from(s_val.as_bytes().len()).unwrap(),
+    ];
     let slice = to_be_signed_sizes_array.into_boxed_slice();
     let mut data_size = 0;
 
@@ -242,7 +286,8 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
         Some(sign_me.as_ptr()),
         slice.as_ptr(),
         ::core::mem::zeroed(),
-        &mut data_size);
+        &mut data_size,
+    );
 
     println!("First call complete. Sign Success:{:?}", sign_success);
 
@@ -256,7 +301,8 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
         Some(sign_me.as_ptr()),
         slice.as_ptr(),
         Some(blob_ptr as *mut u8),
-        &mut data_size);
+        &mut data_size,
+    );
 
     let signed_data = std::slice::from_raw_parts(blob_ptr as *mut u8, data_size as _);
     std::fs::write("foo.pkcs7", signed_data).unwrap();
@@ -264,10 +310,9 @@ pub unsafe fn do_the_signing (fresh_cert:*mut CERT_CONTEXT) {
 
     // clean-up
     CertFreeCertificateChain(fresh_chain);
-
 }
 
-pub unsafe fn convert_from_pem_to_der (pem: &[u8]) -> Result<Vec<u8>> {
+pub unsafe fn convert_from_pem_to_der(pem: &[u8]) -> Result<Vec<u8>> {
     let mut read_len = 0;
     let ok = CryptStringToBinaryA(
         pem,
@@ -278,7 +323,10 @@ pub unsafe fn convert_from_pem_to_der (pem: &[u8]) -> Result<Vec<u8>> {
         ::core::mem::zeroed(),
     );
     if ok == false {
-        return Err(Error::new(GetLastError().to_hresult(), HSTRING::from("Failed converting from PEM to DER part 2")));
+        return Err(Error::new(
+            GetLastError().to_hresult(),
+            HSTRING::from("Failed converting from PEM to DER part 2"),
+        ));
     }
 
     let mut buf = vec![0; read_len as usize];
@@ -292,7 +340,10 @@ pub unsafe fn convert_from_pem_to_der (pem: &[u8]) -> Result<Vec<u8>> {
     );
 
     if ok == false {
-        return Err(Error::new(GetLastError().to_hresult(), HSTRING::from("Failed converting from PEM to DER part 2")));
+        return Err(Error::new(
+            GetLastError().to_hresult(),
+            HSTRING::from("Failed converting from PEM to DER part 2"),
+        ));
     } else {
         return Ok(buf);
     }
