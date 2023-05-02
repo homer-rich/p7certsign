21906a1ae182183ff56470041c4fc7c43652bd51
Homer Rich <homer.rich@X-technologies.com>
2023-05-01 21:20:56 -0400

    Big changes. Subject line printout, each individual run is prompted for from the user and separated the option for each run into the prompt.  Moved some code into functions.  More to come

--- certificates/wcf/README.txt
+++
@@ -1,4 +0,0 @@
-Place all WCF issued certs in this folder
-to run this specific folder use the '-g WCF' option on the createSignedPKCS7s.py script
-Naming convention for certificate [Issuer CN]__[SN]__[Subject CN].cer
-i.e.: DoD_WCF_Root_CA_1__0x164__DoD_WCF_Signing_CA_10.cer.txt
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::{
     path::Component,
 };
 use walkdir::WalkDir;
+use windows::Win32::Security::Cryptography::{CertGetNameStringA, CERT_NAME_SIMPLE_DISPLAY_TYPE};
 use windows::{
     core::*,
     Win32::{
@@ -29,9 +30,6 @@ use windows::{
             UI::{self, CERT_SELECT_STRUCT_W, CSS_ENABLETEMPLATE},
             X509_ASN_ENCODING,
         },
-        Storage::FileSystem::{
-            CreateFileW, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_WRITE, FILE_SHARE_WRITE,
-        },
         System::{
             LibraryLoader::{FreeLibrary, GetProcAddress, LoadLibraryW},
             Memory::{GetProcessHeap, HeapAlloc, HEAP_ZERO_MEMORY},
@@ -41,21 +39,18 @@ use windows::{
 
 // https://learn.microsoft.com/en-us/windows/win32/seccrypto/cryptography-functions
 type CertSelectCertificateW = extern "stdcall" fn(*const CERT_SELECT_STRUCT_W);
+const BUILD_DIRECTORY: &str = "current_build";
 
 fn main() -> Result<()> {
     unsafe {
         let mut fresh_cert: *mut CERT_CONTEXT = ::core::mem::zeroed();
-        select_signing_cert(&mut fresh_cert)?;
-        let mut bundles: HashMap<String, HCERTSTORE> = HashMap::new();
+        let mut bundles: HashMap<String, (String, HCERTSTORE)> = HashMap::new();
+
         let mut current_dir_string: String;
-        if std::fs::metadata("current_build").is_err() {
-            std::fs::create_dir("current_build").unwrap();
-        }
-        let mut readme_string: String =
+        let readme_string_original: String =
             String::from_utf8_lossy(include_bytes!("readme_template.txt")).into();
-        readme_string = readme_string.replace("IRFILENAME", "new_file_name");
-        readme_string = readme_string.replace("SIGNINGCHAIN", "dod_pke_chain.pem");
-        std::fs::write("current_build/README.txt", readme_string.as_bytes()).unwrap();
+
+        select_signing_cert(&mut fresh_cert)?;
 
         for entry in WalkDir::new("certificates")
             .into_iter()
@@ -69,101 +64,119 @@ fn main() -> Result<()> {
                     .to_str()
                     .unwrap()
                     .to_string();
-                let mut temp_store = CertOpenStore(
-                    CERT_STORE_PROV_MEMORY,
-                    CERT_QUERY_ENCODING_TYPE::default(),
-                    HCRYPTPROV_LEGACY::default(),
-                    CERT_OPEN_STORE_FLAGS(0),
-                    ::core::mem::zeroed(),
-                )?;
-                bundles.insert(current_dir_string.clone(), temp_store);
 
-                for certificate_file in entry.path().read_dir().expect("read_dir call failure") {
-                    let current_file = certificate_file.unwrap().path();
-                    if current_file.is_file() && current_file.extension().unwrap() == "cer" {
-                        let cert_context =
-                            get_context_cert_file(&std::fs::read(current_file).unwrap())?;
-                        let update_store = bundles.get(&current_dir_string).unwrap();
-                        let test_add = CertAddCertificateContextToStore(
-                            *update_store,
-                            cert_context,
-                            CERT_STORE_ADD_REPLACE_EXISTING,
-                            None,
-                        );
+                // Reading input from user, will return version number, no, or quit
+                println!("Run bundle for {}? ([Y]es/[N]o/[Q]uit)", current_dir_string);
+                let mut run_bundle = user_input_for_bundle();
 
-                        if test_add.as_bool() {
-                            //println!("Successfully added a component to {}", &current_dir_string);
+                if run_bundle.contains("_") {
+                    let mut temp_store = CertOpenStore(
+                        CERT_STORE_PROV_MEMORY,
+                        CERT_QUERY_ENCODING_TYPE::default(),
+                        HCRYPTPROV_LEGACY::default(),
+                        CERT_OPEN_STORE_FLAGS(0),
+                        ::core::mem::zeroed(),
+                    )?;
+                    bundles.insert(current_dir_string.clone(), (run_bundle, temp_store));
+                    for certificate_file in entry.path().read_dir().expect("read_dir call failure")
+                    {
+                        let current_file = certificate_file.unwrap().path();
+                        if current_file.is_file() && current_file.extension().unwrap() == "cer" {
+                            let cert_context =
+                                get_context_cert_file(&std::fs::read(current_file).unwrap())?;
+                            let update_store = bundles.get(&current_dir_string).unwrap().1;
+                            let test_add = CertAddCertificateContextToStore(
+                                update_store,
+                                cert_context,
+                                CERT_STORE_ADD_REPLACE_EXISTING,
+                                None,
+                            );
+
+                            if test_add.as_bool() {
+                                //let subjectt = std::slice::from_raw_parts(cert_context.read().pCertInfo.read().Subject.pbData, cert_context.read().pCertInfo.read().Subject.cbData as _);
+                                println!("Successfully added {} to {}", get_cert_subject_string(cert_context).unwrap_or("Unknown Cert".to_owned()), &current_dir_string);
+                                //println!("Successfully added {} to {}", String::from_utf8_lossy(subjectt) ,&current_dir_string);
+                                //{:02X?}
+                            }
                         }
                     }
+                } else if run_bundle.contains("q") {
+                    break;
                 }
             }
         }
 
-        let mut collection_store = CertOpenStore(
-            CERT_STORE_PROV_COLLECTION,
-            CERT_QUERY_ENCODING_TYPE::default(),
-            HCRYPTPROV_LEGACY::default(),
-            CERT_OPEN_STORE_FLAGS(0),
-            ::core::mem::zeroed(),
-        )?;
         for y in bundles.into_iter() {
-            let mut file_name = "current_build/certificates_pkcs7_5_12_".to_owned();
+            if std::fs::metadata(BUILD_DIRECTORY).is_err() {
+                std::fs::create_dir(BUILD_DIRECTORY).unwrap();
+            } else {
+                std::fs::remove_dir_all(BUILD_DIRECTORY).unwrap();
+                std::fs::create_dir(BUILD_DIRECTORY).unwrap();
+            }
+
+            let mut file_name = "certificates_pkcs7_v".to_owned();
+            file_name.push_str(y.1 .0.trim());
+            file_name.push('_');
             file_name.push_str(y.0.as_str());
-            file_name.push_str(".p7b\0");
 
-            CertAddStoreToCollection(collection_store, y.1, 0, 0);
+            let mut p7b_file_name = file_name.clone();
+            p7b_file_name.push_str(".p7b\0");
 
+            std::env::set_current_dir(BUILD_DIRECTORY).unwrap();
             // create individual files for each bundle
-            /* let file_name_ptr = PCSTR(file_name.as_ptr()).as_ptr();
+            let p7b_file_name_ptr = PCSTR(p7b_file_name.as_ptr()).as_ptr();
             CertSaveStore(
-                y.1,
+                y.1 .1,
                 PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
                 CERT_STORE_SAVE_AS_PKCS7,
                 CERT_STORE_SAVE_TO_FILENAME_A,
-                file_name_ptr as _,
+                p7b_file_name_ptr as _,
                 0,
-            ); */
+            );
+            let mut readme_string =
+                readme_string_original.replace("IRFILENAME", p7b_file_name.as_str());
+            readme_string = readme_string.replace("SIGNINGCHAIN", "dod_pke_chain.pem");
+            std::fs::write("README.txt", readme_string.as_bytes()).unwrap();
 
-            if !CertCloseStore(y.1, 0).as_bool() {
+            std::env::set_current_dir("..").unwrap();
+            do_the_signing(fresh_cert);
+            zip_up_bundle(file_name);
+
+            if !CertCloseStore(y.1 .1, 0).as_bool() {
                 println!("Failed to close the {} memory_store", y.0);
             }
         }
-        let collection_name = s!("current_build/collection.p7b");
-        CertSaveStore(
-            collection_store,
-            PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
-            CERT_STORE_SAVE_AS_PKCS7,
-            CERT_STORE_SAVE_TO_FILENAME_A,
-            collection_name.as_ptr() as _,
-            0,
-        );
-
-        //do_the_signing(fresh_cert);
-        let mut zipper = std::fs::File::create("zip_zopp.zip").unwrap();
-        let mut zip = zip::ZipWriter::new(zipper);
-        let zip_options = zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
-        
-
-        for entry in WalkDir::new("current_build")
-            .into_iter()
-            .filter_map(|e| e.ok())
-        {
-            if entry.path().is_file() {
-                let current_file = entry.path().file_name().unwrap().to_str().unwrap();
-                //dbg!(current_file);
-                //dbg!(&entry);
-                zip.start_file(current_file, zip_options).unwrap();
-                zip.write_all(&std::fs::read(entry.path().to_str().unwrap()).unwrap()).unwrap();
-            }
-        }
-
-        zip.finish().unwrap();
-
+        // Clean-up
         if !CertFreeCertificateContext(Some(fresh_cert)).as_bool() {
             println!("Failed to close fresh_cert");
         }
     }
+    if std::fs::metadata(BUILD_DIRECTORY).is_ok() {
+        std::fs::remove_dir_all(BUILD_DIRECTORY).unwrap();
+    }
     Ok(())
+}
+
+pub unsafe fn get_cert_subject_string(cert: *mut CERT_CONTEXT) -> Option<String> {
+    let array_size = CertGetNameStringA(
+        cert,
+        CERT_NAME_SIMPLE_DISPLAY_TYPE,
+        ::core::mem::zeroed(),
+        ::core::mem::zeroed(),
+        None
+    );
+    let mut buf:Vec<u8> = vec![0; array_size as usize];
+
+    CertGetNameStringA(
+        cert,
+        CERT_NAME_SIMPLE_DISPLAY_TYPE,
+        ::core::mem::zeroed(),
+        ::core::mem::zeroed(),
+        Some(&mut buf)
+    );
+    
+    let subject_string = String::from_utf8_lossy(&buf).to_string();
+    Some(subject_string)
 }
 
 pub unsafe fn select_signing_cert(fresh_cert: *mut *mut CERT_CONTEXT) -> Result<()> {
@@ -214,11 +227,11 @@ pub unsafe fn select_signing_cert(fresh_cert: *mut *mut CERT_CONTEXT) -> Result<
     }
 
     // clean-up
-    if FreeLibrary(crypt_ui_instance).as_bool() {
-        println!("Closed the lib")
-    };
-    if CertCloseStore(personal_store, 0).as_bool() {
-        println!("Closed the personal_store");
+    if !FreeLibrary(crypt_ui_instance).as_bool() {
+        println!("Failed to close the cryptography library.")
+    }
+    if !CertCloseStore(personal_store, 0).as_bool() {
+        println!("Failed to close the personal_store.");
     }
     Ok(())
 }
@@ -264,13 +277,29 @@ pub unsafe fn do_the_signing(fresh_cert: *mut CERT_CONTEXT) {
     let chain_size = (**(*fresh_chain).rgpChain).cElement;
     let chain_array = std::slice::from_raw_parts(chain_pointer, chain_size as _);
     // get second cert in data array spot 0 is the default cert, slot 1 is next up in the chain
-
+    let chain_size = chain_array.len();
     let intermediate_cert = chain_array[1].pCertContext.cast_mut();
-    let root_cert = *(chain_array[2].pCertContext);
-    let cert_raw_parts =
-        std::slice::from_raw_parts(root_cert.pbCertEncoded, root_cert.cbCertEncoded as _);
-    let binding = convert_from_der_to_pem(cert_raw_parts).unwrap();
-    std::fs::write("current_build/dod_pke_chain.pem", binding.as_bytes()).unwrap();
+    if chain_size > 2 {
+        let root_cert = *(chain_array.last().unwrap().pCertContext);
+        let root_cert_raw_parts =
+            std::slice::from_raw_parts(root_cert.pbCertEncoded, root_cert.cbCertEncoded as _);
+        let root_cert_pem_string = convert_from_der_to_pem(root_cert_raw_parts).unwrap();
+        std::fs::write(
+            "current_build/dod_pke_chain.pem",
+            root_cert_pem_string.as_bytes(),
+        )
+        .unwrap();
+    } else {
+        let intermed_cert_raw_parts =
+            std::slice::from_raw_parts((*intermediate_cert).pbCertEncoded, (*intermediate_cert).cbCertEncoded as _);
+        let intermed_cert_pem_string = convert_from_der_to_pem(intermed_cert_raw_parts).unwrap();
+        std::fs::write(
+            "current_build/intermed_pke_chain.pem",
+            intermed_cert_pem_string.as_bytes(),
+        )
+        .unwrap();
+    }
+    
 
     // get chain size
     let mut certs_in_signature: Vec<*mut CERT_CONTEXT> = vec![intermediate_cert, fresh_cert];
@@ -301,12 +330,12 @@ pub unsafe fn do_the_signing(fresh_cert: *mut CERT_CONTEXT) {
     };
 
     let mut sha256_string = String::new();
-    for entry in WalkDir::new(".")
+    for entry in WalkDir::new(BUILD_DIRECTORY)
         .max_depth(1)
         .into_iter()
         .filter_map(|e| e.ok())
     {
-        if entry.path().is_file() && entry.path().extension().unwrap_or_default() == "p7b" {
+        if entry.path().is_file() {
             let input = std::path::Path::new(entry.path().as_os_str());
             let path_string = entry.path().file_name().unwrap().to_str().unwrap();
             let val = sha256::try_digest(input).unwrap();
@@ -420,5 +449,66 @@ pub unsafe fn convert_from_der_to_pem(der: &[u8]) -> Result<String> {
         ));
     } else {
         Ok(pstr_buf.to_string().unwrap())
+    }
+}
+
+pub unsafe fn zip_up_bundle(file_name: String) {
+    let mut zip_file_name = file_name;
+    zip_file_name.push_str(".zip");
+    let mut zipper = std::fs::File::create(zip_file_name).unwrap();
+    let mut zip = zip::ZipWriter::new(zipper);
+    let zip_options =
+        zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
+
+    for entry in WalkDir::new(BUILD_DIRECTORY)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if entry.path().is_file() {
+            let current_file = entry.path().file_name().unwrap().to_str().unwrap();
+            zip.start_file(current_file, zip_options).unwrap();
+            zip.write_all(&std::fs::read(entry.path().to_str().unwrap()).unwrap())
+                .unwrap();
+        }
+    }
+
+    zip.finish().unwrap();
+}
+
+pub unsafe fn user_input_for_bundle() -> String {
+    let mut input = String::new();
+    loop {
+        input.clear();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input.");
+        input = input.to_lowercase();
+        if input.contains("yes") || input.contains("y") {
+            input.clear();
+            println!("What bundle number would for this bundle?  i.e. 5_2 or 1_2_345");
+            loop {
+                std::io::stdin()
+                    .read_line(&mut input)
+                    .expect("Failed to read input.");
+                if input.split("_").count() > 1
+                    && input
+                        .split("_")
+                        .all(|num| num.trim().parse::<u32>().is_ok())
+                {
+                    return input;
+                }
+                println!(
+                    "Error with input, must be at least two numbers seperated by an underscore."
+                );
+                input.clear();
+            }
+        } else if input.contains("no")
+            || input.contains("n")
+            || input.contains("quit")
+            || input.contains("q")
+        {
+            return input;
+        }
+        println!("Error with input, must be one of these three options ([Y]es/[N]o/[Q]uit)")
     }
 }
